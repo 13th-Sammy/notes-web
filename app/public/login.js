17b3489b6774e89b3ba350d5a22a2ddff8a2360f
13th-Sammy <213174535+13th-Sammy@users.noreply.github.com>
2025-07-11 13:51:09 +0530

    Update

---
+++ app/public/login.js
@@ -0,0 +1,34 @@
+import {post} from './api.js';
+
+const okBtn=document.getElementById("ok");
+let submitted=false;
+
+async function login(event) {
+    event.preventDefault();
+    if(submitted) return;
+    submitted=true;
+    setTimeout(() => submitted = false, 1000);
+
+    const username=document.getElementById("uname").value.trim();
+    const password=document.getElementById("pword").value.trim();
+
+    if(!username || !password) {
+        alert("Username and Password cannot be empty");
+        return;
+    }
+
+    try {
+        const response = await post("/login", {username, password});
+        if(response.success) {
+            window.location.href="notes.html";
+        }
+        else {
+            alert(response.message || "Login Failed");
+        }
+    } catch (e) {
+        alert("Error: "+e?.message||e);
+    }
+}
+
+okBtn.addEventListener("click", login);
+okBtn.addEventListener("touchstart", login);
